a652626fa0ab1328dc6838de0b3533545213f487
Anwesha Mishra <anwesharoses@gmail.com>
2024-01-24 10:38:43 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthorizationMiddleware } from './auth/controller/authorization.middleware';
 import { HttpException, HttpStatus, ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Apply CORS middleware with all origins
+  app.use(
+    cors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    }),
+  );
   // Apply the ValidationPipe globally
   app.useGlobalPipes(
     new ValidationPipe({
